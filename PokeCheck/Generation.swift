dfa07112d5c0e86ebaa215eb278f0f19e002b57e
Ike Mattice <gogreen37@me.com>
2016-10-17 08:44:29 -0700

    add base classes

---
+++ PokeCheck/Generation.swift
@@ -0,0 +1,13 @@
+//
+//  Generation.swift
+//  PokeCheck
+//
+//  Created by Ike Mattice on 10/16/16.
+//  Copyright © 2016 Ike Mattice. All rights reserved.
+//
+
+import Foundation
+
+class Generation {
+    
+}

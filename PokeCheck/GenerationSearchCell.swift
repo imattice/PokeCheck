7ad6000fa5ceeea8b36055611de3a818036f32e8
Ike <ike.mattice@tiger-sheep.com>
2016-11-29 16:59:38 -0800

    add draft nib files for filter menu.

---
+++ PokeCheck/GenerationSearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  GenerationSearchCell.swift
+//  PokeCheck
+//
+//  Created by Ike Mattice on 11/29/16.
+//  Copyright © 2016 Ike Mattice. All rights reserved.
+//
+
+import UIKit
+
+class GenerationSearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ PokeCheck/NameSearchCell.swift
@@ -0,0 +1,24 @@
+//
+//  NameSearchCell.swift
+//  PokeCheck
+//
+//  Created by Ike Mattice on 11/29/16.
+//  Copyright © 2016 Ike Mattice. All rights reserved.
+//
+
+import UIKit
+
+class NameSearchCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
